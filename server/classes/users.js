48a53cf02f37f7fbb7ab16a46c7fc6ddfa9cc6d9
Daniela Liendo <daniela@ualabee.com>
2023-02-04 23:58:41 -0300

    getUser method and its documentation were created in user class

--- server/classes/users.js
+++ server/classes/users.js
@@ -1,14 +1,33 @@
 class Users {
 
   constructor() {
-    this.users = [];
+    this.users = []; // Users list
   }
 
+  /** addUser()
+   * Add new user to user list
+   * @param {String} id
+   * @param {String} name
+   * @returns {[]}
+   */
   addUser(id, name) {
-    const user = { id, name }
+    const user = {id, name}
     this.users.push(user)
 
     return this.users
+  }
+
+  /** getUser()
+   * Get user by id from users list
+   * @param {String} id
+   * @returns { Object | null}
+   */
+  getUser(id) {
+
+    const user = this.users.find(user => user.id === id);
+
+    return user ? user : null
+
   }
 
 }
